9956417c9f4a978f069e0ca50cb966e164caa2d2
Irvin Shen <irvinshen.texas@gmail.com>
2020-11-17 10:29:29 -0600

    Created (empty) README and updated to comment out key-specific hue changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
 
     screen = pygame.display.set_mode((1080, 720))
     hue = 0
-    sat = 50
+    sat = 100
     lumin = 50
     alpha = 100
     color = pygame.Color(255, 255, 255)
@@ -22,10 +22,10 @@ def main():
     while running:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    hue = (hue + 5) % 360
-                if event.key == pygame.K_0:
-                    hue = (hue - 5) % 360
+                # if event.key == pygame.K_a:
+                #     hue = (hue + 5) % 360
+                # if event.key == pygame.K_0:
+                hue = (hue - .5) % 360
             color.hsla = (hue, sat, lumin, alpha)
             screen.fill(color)
             if event.type == pygame.QUIT:
