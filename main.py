9d59381b725f931b22d21797393f95672a7c26d8
Irvin Shen <irvinshen.texas@gmail.com>
2021-05-26 19:54:42 -0500

    Fixed the color change having a strange lag by moving the color updating outside of the event-checking for loop. Also added a small menu using the pygame-menu lib.

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import pygame
+import pygame_menu
+
 
 class keyLight():
     def __init__(self, keyValue):
         keyLight.key = keyValue
 
 
-
-
-
-def main():
-    pygame.init()
-
-    logo = pygame.image.load("logo_32_32.png")
-    pygame.display.set_icon(logo)
-    pygame.display.set_caption("Rainbow Keyboard Game")
-
-    screen = pygame.display.set_mode((1080, 720))
+def start_the_game():
+    display = pygame.display.set_mode((1080, 720))
     hue = 0
     sat = 100
     lumin = 50
     alpha = 100
     color = pygame.Color(255, 255, 255)
     color.hsla = (hue, sat, lumin, alpha)
-    screen.fill(color)
+    display.fill(color)
     pygame.display.flip()
     # Create main loop variable and game loop
     running = True
@@ -36,13 +29,34 @@ def main():
                 keyDown = True
             if event.type == pygame.KEYUP:
                 keyDown = False
-            color.hsla = (hue, sat, lumin, alpha)
-            screen.fill(color)
             if event.type == pygame.QUIT:
                 running = False
         if keyDown:
-            hue = (hue - .07) % 360
+            hue = (hue - .04) % 360
+            pygame.display.update()
+        color.hsla = (hue, sat, lumin, alpha)
+        display.fill(color)
         pygame.display.update()
+    pass
+
+
+def main():
+    pygame.init()
+
+    logo = pygame.image.load("logo_32_32.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("Rainbow Keyboard Game")
+
+    screen = pygame.display.set_mode((1080, 720))
+
+    menu = pygame_menu.Menu('RainbowBoard', 1080, 720,
+                            theme=pygame_menu.themes.THEME_DARK)
+
+    menu.add.text_input('Name: ', default='Matthew')
+    menu.add.button('Play', start_the_game)
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+
+    menu.mainloop(screen)
 
 
 if __name__ == "__main__":
