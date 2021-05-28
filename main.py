fb709d6ad3b46362ce8b52815c44498489d68acd
Irvin Shen <irvinshen.texas@gmail.com>
2021-05-28 00:10:37 -0500

    Added a Surface and SubSurfaces so that I can confirm that changing multiple areas at once works. Next steps are creating functions and objects to better organize work instead of copy and pasting work over and over. Currently working with 2 subsurfaces.

--- main.py
+++ main.py
@@ -8,34 +8,59 @@ class keyLight():
 
 
 def start_the_game():
-    display = pygame.display.set_mode((1080, 720))
-    hue = 0
+    # Set up surfaces
+    screen = pygame.display.set_mode((1080, 720))
+    canvas = pygame.Surface((1080, 720))
+    left_side = pygame.Rect(0, 0, 540, 720)
+    right_side = pygame.Rect(540, 0, 540, 720)
+    sub_left = canvas.subsurface(left_side)
+    sub_right = canvas.subsurface(right_side)
+
+    screen.blit(canvas, [0,0])
+    screen.blit(sub_right, [540, 0])
+    screen.blit(sub_left, [0, 0])
+
+    left_hue = 0
+    right_hue = 0
     sat = 100
     lumin = 50
     alpha = 100
-    color = pygame.Color(255, 255, 255)
-    color.hsla = (hue, sat, lumin, alpha)
-    display.fill(color)
+    left_color = pygame.Color(255, 255, 255)
+    left_color.hsla = (left_hue, sat, lumin, alpha)
+    sub_left.fill(left_color)
+    right_color = pygame.Color(255, 255, 255)
+    right_color.hsla = (right_hue, sat, lumin, alpha)
+    sub_right.fill(right_color)
+
     pygame.display.flip()
+
     # Create main loop variable and game loop
     running = True
-    keyDown = False
+    left_key_down = False
+    right_key_down = False
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                # if event.key == pygame.K_a:
-                #     hue = (hue + 5) % 360
-                # if event.key == pygame.K_0:
-                keyDown = True
-            if event.type == pygame.KEYUP:
-                keyDown = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                left_key_down = True
+            elif event.type == pygame.KEYUP and event.key == pygame.K_q:
+                left_key_down = False
+            elif event.type == pygame.KEYDOWN:
+                right_key_down = True
+            elif event.type == pygame.KEYUP:
+                right_key_down = False
+
             if event.type == pygame.QUIT:
                 running = False
-        if keyDown:
-            hue = (hue - .04) % 360
-            pygame.display.update()
-        color.hsla = (hue, sat, lumin, alpha)
-        display.fill(color)
+
+        if left_key_down:
+            left_hue = (left_hue - .12) % 360
+            left_color.hsla = (left_hue, sat, lumin, alpha)
+            sub_left.fill(left_color)
+        if right_key_down:
+            right_hue = (right_hue - .12) % 360
+            right_color.hsla = (right_hue, sat, lumin, alpha)
+            sub_right.fill(right_color)
+        screen.blit(canvas, [0, 0])
         pygame.display.update()
     pass
 
